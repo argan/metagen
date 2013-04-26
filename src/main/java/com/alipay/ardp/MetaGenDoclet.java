8460f2fbb5b0a0eb2579e4a4510112867beee856
Argan Wang <argan_w@yahoo.com.cn>
2013-04-26 13:56:38 +0800

    add languageVersion

--- src/main/java/com/alipay/ardp/MetaGenDoclet.java
+++ src/main/java/com/alipay/ardp/MetaGenDoclet.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 
 import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.LanguageVersion;
 import com.sun.javadoc.RootDoc;
 
 /**
@@ -93,4 +94,8 @@ public class MetaGenDoclet {
         return 0;
     }
 
+    public static LanguageVersion languageVersion() {
+        return LanguageVersion.JAVA_1_5;
+    }
+    
 }
